bae14347d5dfefaa2ab561b79b73e6fb47724a22
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-06-30 10:16:29 +0200

    Upload the RIGHT files

--- main.js
+++ main.js
@@ -1,48 +1,107 @@
 'use strict'
 
-// generar un número al azar entre 1 y 100, y la jugadora tiene que adivinarlo.
-
-// El juego da pistas sobre si el nº que prueba es demasiado alto o bajo y contabiliza el nº de intentos hasta que al final la jugadora acierta el juego
-
-
-// Cuando la jugadora introduzca un número mayor que el aleatorio y pulse en Prueba: 
-
-// Cuando la jugadora no introduzca un número válido y pulse en Prueba: El número debe estar entre 1 y 100.
+/*-----------------------------------*
+ *  Constantes / Variables globales  *
+ *-----------------------------------*/
+// Referencia al botón y al número random
 const button = document.querySelector('.btn');
-
-
-
 const numberToGuess = getRandomNumber(100);
-console.log(numberToGuess);
+console.log(`El número aleatorio es: ${numberToGuess}`); //  para verificar
 
+// Variables para la función checkNumber
+let attemptMesg = document.querySelector('.attempts');
+let attempts = 0;
+let userNumber;
+
+/*-----------------------------------*
+ *   Función para número aleatorio   *
+*------------------------------------*/
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
-let userNumber;
+
+
+/*-----------------------------------*
+ *   Función para comparar números   *
+ *-----------------------------------*/
 function checkNumber() {
-    const userNumber = document.querySelector('#chosen-number');
-    let attempts;
+    attempts++;
+    // Constantes / Variables locales
+    userNumber = document.querySelector('#chosen-number');
     let hint = document.querySelector('.hint');
 
+    hint.className = 'hint'; // quitar todas las clases extra de hint
 
+    // Error!
     if (userNumber.value < 1 || userNumber.value > 100) {
         hint.innerHTML = "El número debe estar entre 1 y 100.";
-    } else if (userNumber.value == numberToGuess) {
-        hint.innerHTML = "Has ganado campeona!!!";
+        hint.classList.add('alert-msg');
+    // Acierto!
+    } else if (parseInt(userNumber.value) === parseInt(numberToGuess)) {
+        hint.innerHTML = "Has ganado campeona!!!🎉";
+        hint.classList.add('special-msg');
+        startAgain(); // Reinicia el juego
+    // Demasiado bajo! 
     } else if (userNumber.value < numberToGuess) {
-        hint.innerHTML = "Demasiado bajo.";
+        hint.innerHTML = 'Demasiado bajo. <span id="icon"></span>'; // crea icono
+        iconHint(); // Aporta ayuda extra (cliente/frío)
+    // Demasiado alto!
     } else if (userNumber.value > numberToGuess) {
-        hint.innerHTML = "Demasiado alto.";
-
+        hint.innerHTML = 'Demasiado alto. <span id="icon"></span>'; // crea icono
+        iconHint(); // Aporta ayuda extra (cliente/frío)
     }
 
+    attemptMesg.innerHTML = `Número de intentos: ${attempts}`;
 
+    /*-----------------------------------------------*
+    *   Extra: icono cuando se está frío/caliente   *
+    *-----------------------------------------------*/
+    // Esta funcionalidad extra añade un icono de fuego cuando el número elegido por el usuario es cercano al número aleatorio (10) y un icono de hielo cuando hay más de 20 números de diferencia.
+    // Para ello creo nueva variable que será la diferencia entre el número elegido y el random:
+    function iconHint() {
+        let icon = document.querySelector('#icon'); // icono dentro de hint
+        const numberDif = Math.abs(numberToGuess - userNumber.value); 
+        console.log("La diferencia entre el número elegido y el aleatorio es de: " + numberDif);
+        
+        // Calentito!
+        if (numberDif <= 10) {
+            icon.innerHTML = '🔥';
+            hint.classList.add('hot');
+            console.log("Caliente, caliente");
+
+        // Frío!
+        } else if (numberDif > 20) {
+            icon.innerHTML = '🧊';
+            hint.classList.add('cold');
+            console.log("¡Frío!");
+        
+        // Templado!
+        } else {
+            icon.innerHTML = '';
+        }
+    }
 
 }
     
 
 
-
-
+// Event Listener en el botón
 button.addEventListener('click', checkNumber);
 
+
+
+
+
+/*-----------------------------------------------*
+ *          Extra: volver a empezar              *
+ *-----------------------------------------------*/
+// Que cuando el usuario acierte y presione sobre el botón, la página se refresque para volver a jogar
+function startAgain() {
+    button.innerHTML = "Vuelve a empezar";
+    button.addEventListener('click', () => {
+        location.reload();
+        userNumber.value = '';
+    });
+
+    
+}
